5f3fb9b324f0f516d83d400871c6b5994973ccc8
jhpark223 <thesound08@naver.com>
2023-03-09 20:54:57 +0900

    initial version of main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def twoSum(self, nums: List[int], target: int) ->List[int]:
-  def twoSum(nums: List[int], target: int) -> List[int]:
+ def twoSum(nums: List[int], target: int) -> List[int]:
       
     
     for i in range(1, len(nums)):
         for j in range(i):
             if nums[i] + nums[j] == target:
-                return [i,j]
-
+                return [i,j] 
